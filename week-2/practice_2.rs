0f1130b05b9665d82e34b3e2edffbb47656f59f8
Semilore ✬ <user@Semilores-MacBook-Pro.local>
2026-09-02 20:16:08 +0100

    Adding week 2 folder and Rust practice and projects

---
+++ week-2/practice_2.rs
@@ -0,0 +1,7 @@
+fn main() {
+	println!("Welcome"); //testing comments
+
+	println!("My name is {}, I am a programmer", "Bamgboye Samuel");
+
+	println!("Format {} documents", "all"); //{} is a placeholder.
+}
