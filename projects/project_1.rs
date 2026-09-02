87749fdc6ae6529dbe85fc06a6a303b4bb7e1ab8
Semilore ✬ <user@Semilores-MacBook-Pro.local>
2026-09-02 21:58:29 +0100

    Update project 1

--- projects/project_1.rs
+++ projects/project_1.rs
@@ -1,10 +1,10 @@
 fn main(){
 	let p:f64 = 520_000_000.00;
 	let r:f64 = 10.0;
-	let t:f64 = 5.0;
+	let t:i32 = 5;
 
 	//amount 
-	let a = p * (1.0 + (r / 100.0)) * t;
+	let a = p * (1.0 + (r / 100.0)).powi(t);
 
 	//Compound interest
 	let c = a - p;
